300469fce71148e4a1e8eb86300fddf8e7efe920
rutu1012 <rutushrirame@gmail.com>
2022-06-27 15:41:48 +0530

    Save model option

--- main_gan.py
+++ main_gan.py
@@ -5,10 +5,11 @@ from trainNtest import training, testing
 from utility import cost_graph, view_images
 import loader
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context #to downld cifar10
+
+ssl._create_default_https_context = ssl._create_unverified_context  # to downld cifar10
 
 batch_size = 64
-num_epochs = 5
+num_epochs = 10
 train_loader = loader.trainLoader(batch_size)
 test_loader = loader.testLoader(batch_size)
 lr = 1e-5
@@ -26,7 +27,7 @@ def main(training_data, testing_data, num_epochs):
     FILE = "dcgan.pt"
     ch = input("Press l to load model, t to train model: ").lower()  # asks user if they want to train the model or load the already saved model
     if ch == 'l':
-        model.load_state_dict(torch.load(FILE,map_location=device))  # loads the model
+        model.load_state_dict(torch.load(FILE, map_location=device))  # loads the model
         # test the loaded model on the Test data
         test_outputs, dloss, gloss = testing(model, test_loader, num_epochs)
         cost_graph(dloss, gloss, "DCGAN  Test Loss")
@@ -34,13 +35,9 @@ def main(training_data, testing_data, num_epochs):
     elif ch == 't':
         train_outputs, d_loss, g_loss = training(model, train_loader, num_epochs, g_optimizer, d_optimizer)
         cost_graph(d_loss, g_loss, "DCGAN  Train Loss")
-
-        # view_images(train_outputs, num_epochs)
-        # torch.save(model.state_dict(), FILE)  # saves the trained model at the specified path
-
-    # files.download('VAE_model.pth') colab
-
-    # view_images(test_outputs, num_epochs)
+        s = input("Model has been trained do you want to save it? (y/n): ").lower()
+        if s == 'y':
+            torch.save(model.state_dict(), FILE)  # saves the trained model at the specified path
 
 
 main(train_loader, test_loader, num_epochs)
